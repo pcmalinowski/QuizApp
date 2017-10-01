8cef1ac19b5b9379f50811cc444c5ee8038037c3
pcmalinowski <pcmalinowski@gmail.com>
2017-10-01 12:05:57 -0500

    second commit

--- index.js
+++ index.js
@@ -1,65 +1,152 @@
 $(function () {
   startQuiz();
-  handleCorrectAnswerInfo();
   handleAnswerSubmitted();
-  renderQuestionBox();
+  findQuestion();
 });
 
 var choose = {
 	questions: [
 	{
-      question: ``,
-      answers: ['', '', '', ''],
-      correctAnswer: ``
+      question: `What character does Jim ask out by leaving her a voicemail?`,
+      answers: ['Pam', 'Katy', 'Brenda', 'Karen'],
+      rightAnswer: `Brenda`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `How did the chair model die?`,
+      answers: [`Car Crash`, `Overdose`, `Skydiving`, `Brushing her teeth`],
+      rightAnswer: `Car Crash`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What is the name of Michael's computer?`,
+      answers: [`Howard`, `Frank`, `Chevy`, `Harvey`],
+      rightAnswer: `Harvey`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `Where does Jim say you can buy gaydar?`,
+      answers: [`Brookstone`, `Sharper Image`, `Skymall`, `RadioShack`],
+      rightAnswer: `Sharper Image`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What soccer team was Jim on in 4th Grade?`,
+      answers: [`The Orange Team`, `The Red Team`, `The Lightning`, `The Chargers`],
+      rightAnswer: `The Orange Team`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What is the name of the file of Jan's racy vacation photo?`,
+      answers: [`HOT`, `Jamaican Jan Sun Princess`, `Jamaican Jan Paradise`, `Vacation Jan By Micheal Scott`],
+      rightAnswer: `Jamaican Jan Sun Princess`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What device does Pam use to time Dwights laps running around the building?`,
+      answers: [`Digital Thermometer`, `Computer Mouse`, `Stopwatch`, `Cellphone`],
+      rightAnswer: `Digital Thermometer`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What part Native American does Micheal claim to be?`,
+      answers: [`1/6`, `1/3`, `2/15`, `4/18`],
+      rightAnswer: `2/15`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `During his survival mission, which of these did Micheal not take into the woods?`,
+      answers: [`Camera`, `Lighter`, `Knife`, `Bag of Cereal`],
+      rightAnswer: `Lighter`
     },
     {
-      question: ``,
-      answers: [``, ``, ``, ``],
-      correctAnswer: ``
+      question: `What type of meat is in Dwight's meat sandwich?`,
+      answers: [`Alligator`, `Pork`, `Pony`, `Goat`],
+      rightAnswer: `Pony`
     }
 	],
 
-questionIndex: 0,
-totalCorrect: 0,
+questionList: 0,
+numberRight: 0,
 
 };
+
+function startQuiz() {
+  $('#start-button').click(function (event) {
+    $('#quiz-text').removeClass('hidden');
+    $('#start-button').addClass('hidden');
+  });
+}
+
+function resetQuiz() {
+  choose.numberRight = 0;
+  choose.questionList = 0;
+}
+
+function findQuestion() {
+  var questionObj = choose.questions[choose.questionList];
+  creatQuestionText();
+  renderQuestionChoices(questionObj.answers);
+}
+
+function creatQuestionText() {
+  var changeHTML = '<span>(' + (choose.questionList + 1) + '/' + choose.questions.length + ')</span>'
+  var questionText = choose.questions[choose.questionList].question;
+  $('.js-question-text').html(changeHTML + questionText);
+}
+
+function renderQuestionChoices(answers) {
+  $('#questions-list label').each(function (index, label) {
+    $(this).find('input').attr('value', answers[index]);
+    $(this).find('input').prop('checked', false);
+    $(this).find('span').text(answers[index]);
+  });
+}
+
+function handleAnswerSubmitted() {
+  $('#submit-choice').click(function (event) {
+    event.preventDefault();
+    var userPick = $('input[name="answerSelect"]:checked').val();
+    checkAnswer(userPick);
+  });
+}
+
+function checkAnswer(userPick) {
+  var correctChoice = choose.questions[choose.questionList].rightAnswer;
+  if (userPick == correctChoice) {
+    choose.numberRight++;
+    renderQuestionFeedback(true);
+    choose.questionList++;
+  } else if(userPick == undefined){
+    renderQuestionFeedback('unanswered');
+  } else {
+    renderQuestionFeedback(false);
+    choose.questionList++;
+  }
+  if (choose.questionList == choose.questions.length) {
+    renderFinalResults();
+  } else {
+    findQuestion();
+  }
+}
+
+function renderQuestionFeedback(boolean) {
+  var feedback = $('.answer-feedback');
+  if (boolean == true){
+    feedback.find('h3').text('Yeshhh!');
+  } else if (boolean == false){
+    feedback.find('h3').text('Toby, you are the worst.');
+  } else if (boolean == 'unanswered'){
+    feedback.find('h3').text('I will just wait till everyone goes home to make a decision.');
+  }
+}
+
+function renderFinalResults() {
+  $('quiz-text').addClass('hidden');
+  $('#restart-quiz').removeClass('hidden');
+  var element = $('.js-quiz-results');
+  element.html('<h2>' + 'You answered ' + choose.numberRight + ' of ' + choose.questions.length + ' right!' + '</h2>');
+  handleQuizRestart();
+}
+
+function handleQuizRestart() {
+  $('#restart-quiz').on('click', function (event) {
+    $('#quiz-text').removeClass('hidden');
+    $('#restart-quiz').addClass('hidden');
+    $('.js-quiz-results').text('');
+    resetQuiz();
+    findQuestion();
+  });
+}
